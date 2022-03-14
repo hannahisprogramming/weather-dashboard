9bac0e575c0310582c8286ff1d3d88ef5cc6abd3
Hannah Bush <heb.rutgers@gmail.com>
2022-03-14 17:23:14 -0400

    prevent default

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//comment to initialize
+var submit = document.getElementById("submit");
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
+  var cityName = document.getElementById("cityName").value;
+  console.log(cityName);
+});
