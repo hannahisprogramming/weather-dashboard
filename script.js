a750d95b0d97bda5f36443125f77ac51e10996c7
Hannah Bush <heb.rutgers@gmail.com>
2022-05-06 13:12:29 -0400

    got api key, found lat and long from api

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+const apiKey = "5b69c3d8f369d5f1a210756576ee1267";
+
+
 var submit = document.getElementById("submit");
 submit.addEventListener("click", (event) => {
   event.preventDefault();
   var cityName = document.getElementById("cityName").value;
   console.log(cityName);
+  weatherLatLong(cityName);
 });
+
+
+function weatherLatLong (cityName) {
+  var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+  fetch(url)
+  .then(result => result.json())
+  .then((apiResponse) => {
+    console.log(apiResponse);
+    var lat = apiResponse.coord.lat;
+    var long = apiResponse.coord.lon;
+  })
+}
